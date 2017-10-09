b0407cb39be8bd2decbfd32c7a6b57024eb39af1
João Conde <joaoconde_13@hotmail.com>
2017-10-09 12:29:52 +0100

    New textures added, planets resized and moved, shuttle implemented

--- laig-reader/main.js
+++ laig-reader/main.js
@@ -32,7 +32,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	var filename=getUrlVars()['file'] || "shuttle.xml";
+	var filename=getUrlVars()['file'] || "solar_system.xml";
 
 
 	// create and load graph, and associate it to scene.
